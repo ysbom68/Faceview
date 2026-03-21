7d2837f70337e981c86cd3b3fabb5463d3f08cf1
ysbom68 <ysbom@proton.me>
2026-03-21 05:43:01 +0000

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function changeImage() {
-  const img = document.getElementById("randomImage");
-
-  // Generate a random number to prevent caching
-  const randomNum = Math.floor(Math.random() * 1000);
-
-  img.src = `https://picsum.photos/400?random=${randomNum}`;
-}
