76da598373d9bbbe2c9ccc2c6e7c336e0cdbd970
ysbom68 <ysbom@proton.me>
2026-03-21 05:36:00 +0000

    Add function to change image with random source

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+function changeImage() {
+  const img = document.getElementById("randomImage");
 
+  // Generate a random number to prevent caching
+  const randomNum = Math.floor(Math.random() * 1000);
+
+  img.src = `https://picsum.photos/400?random=${randomNum}`;
+}
